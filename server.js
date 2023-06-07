70dddc23b69c355c7d060066c9824b8da2fa2204
AlexTheBestCoder <119418000+AlexTheBestCoder@users.noreply.github.com>
2023-06-07 10:58:37 +0000

    Update server.js

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ app.use(cors({
   credentials: true
 }));
 app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', 'https://bima-room-frontend.vercel.app/'); // Remplacez par l'URL de votre frontend
+  res.setHeader('Access-Control-Allow-Origin', '*'); // Remplacez par l'URL de votre frontend
   res.setHeader('Access-Control-Allow-Credentials', 'true');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization,');
